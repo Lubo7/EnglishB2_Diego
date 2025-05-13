fa8210c21e181db713abac1434ca463a2288f541
Lubo7 <Lkramoris@gmail.com>
2025-05-13 19:28:38 +0200

    Test: simplify app.tsx and main.tsx for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
+//import './index.css';
+//import { StrictMode } from 'react';
+//import { createRoot } from 'react-dom/client';
+//import App from './App.tsx';
+//import { ProgressProvider } from './components/ProgressContext.tsx';
+
+//createRoot(document.getElementById('root')!).render(
+  //<StrictMode>
+    //<ProgressProvider>
+      //<App />
+    //</ProgressProvider>
+  //</StrictMode>
+//);
+
+// src/main.tsx
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import { ProgressProvider } from './components/ProgressContext.tsx';
+import App from './App.tsx'; // This will be the super simplified App
+// import { ProgressProvider } from './components/ProgressContext.tsx'; // Comment out for now
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ProgressProvider>
+    {/* <ProgressProvider> */} {/* Comment out for now */}
       <App />
-    </ProgressProvider>
+    {/* </ProgressProvider> */}
   </StrictMode>
 );
